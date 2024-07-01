554c5cc6b269641d05d2c47f05a06518a492af14
MNxMunno <bilolhuzayfa@gmail.com>
2024-07-02 01:36:56 +0500

    change

--- src/components/nornlight/Nornlight.jsx
+++ src/components/nornlight/Nornlight.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Btn from "../../static/btn/Btn";
+import { FaMedal } from "react-icons/fa6";
+import { MdPriceChange } from "react-icons/md";
+import { TbTruckDelivery } from "react-icons/tb";
+import { GrCatalog } from "react-icons/gr";
 
 const Nornlight = () => {
   return (
@@ -10,7 +14,36 @@ const Nornlight = () => {
             <h1>Почему NORNLIGHT?</h1>
             <Btn text={"О компании"} />
           </div>
-          <div className="cards"></div>
+          <div className="cards">
+            <div className="card">
+              <div className="img">
+                <FaMedal />
+              </div>{" "}
+              <h3>Только проверенные бренды</h3>
+              <p>Бренды, проверенные временем и качествомx</p>
+            </div>
+            <div className="card">
+              <div className="img">
+                <MdPriceChange />
+              </div>{" "}
+              <h3>Самые низкие цены</h3>
+              <p>Ниже не будет нигде</p>
+            </div>
+            <div className="card">
+              <div className="img">
+                <TbTruckDelivery />
+              </div>{" "}
+              <h3>Быстрая доствка</h3>
+              <p>Доставляем по всей РФза 1-10 дней</p>
+            </div>
+            <div className="card">
+              <div className="img">
+                <GrCatalog />
+              </div>{" "}
+              <h3>Большой ассортимент</h3>
+              <p>Более 1000 товаров</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
